351c9240a29718527182b43b44ff73c6122b1753
rfahham <rfahham@hotmail.com>
2024-05-29 11:00:31 -0300

    terraform x opentofu

--- README.md
+++ README.md
@@ -25,4 +25,50 @@ OpenTofu v1.7.1
 on linux_amd64
 ```
 
+## Executando o terraform
+
+```bash
+terraform init
+
+terraform plan
+
+terraform apply
+```
+
+### OBS
+
+Será criada as pastas .terraform/providers/registry.terraform.io
+
+## Verificando criação do pods
+
+```bash
+ kubectl get pods                      
+NAME                                  READY   STATUS    RESTARTS   AGE
+nginx-deployment-2-79cbb6b976-dpbcm   1/1     Running   0          71s
+nginx-deployment-2-79cbb6b976-mx5ql   1/1     Running   0          71s
+```
+
+
+## Verificando o deploy
+
+```bash
+kubectl get deploy          
+NAME                 READY   UP-TO-DATE   AVAILABLE   AGE
+nginx-deployment     2/2     2            2           47s
+
+```
+
+## Executando a conversão
+
+```bash
+tofu init
+
+tofu plan
+
+tofu apply
+```
+
+### OBS
+
+Será criada as pastas .terraform/providers/registry.opentofu.org
 
--- deploy-nginx.tf
+++ deploy-nginx.tf
@@ -1,44 +1,44 @@
 provider "kubernetes" {
-    config_path = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
 
 resource "kubernetes_deployment" "nginx" {
-    metadata {
-        name = "nginx-deployment"
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
         labels = {
-            app = "nginx"
+          app = "nginx"
         }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
     }
-
-    spec {
-        replicas = 2
-        selector {
-            match_lables = {
-                app = "nginx"
-            }
-        }
-
-        template {
-            metadata {
-               labels = {
-                  app = "nginx"
-                } 
-            }
-
-            spec {
-                container {
-                    image = "nginx-latest"
-                    name = "nginx"
-
-                    port {
-                        container_port = 80
-                    }
-                }
-            }
-        }
-    }
+  }
 }
 
 output "nginx_deployment_name" {
-    value = kubernetes_deployment.nginx.metadata[0].name
+  value = kubernetes_deployment.nginx.metadata[0].name
 }
