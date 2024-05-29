f7fef9b47d01cf2c0405423865b3f31acc6fe8e3
Ricardo Fahham <rfahham@gmail.com>
2024-05-29 10:28:56 -0300

    deploy terraform

---
+++ deploy-nginx.tf
@@ -0,0 +1,44 @@
+provider "kubernetes" {
+    config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "nginx" {
+    metadata {
+        name = "nginx-deployment"
+        labels = {
+            app = "nginx"
+        }
+    }
+
+    spec {
+        replicas = 2
+        selector {
+            match_lables = {
+                app = "nginx"
+            }
+        }
+
+        template {
+            metadata {
+               labels = {
+                  app = "nginx"
+                } 
+            }
+
+            spec {
+                container {
+                    image = "nginx-latest"
+                    name = "nginx"
+
+                    port {
+                        container_port = 80
+                    }
+                }
+            }
+        }
+    }
+}
+
+output "nginx_deployment_name" {
+    value = kubernetes_deployment.nginx.metadata[0].name
+}
